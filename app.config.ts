255cadaddd8b43a7944c4d7ed2d65c76f968b7ad
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-02-08 15:32:55 +0000

    create testing welcome page

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   docus: {
     title: 'MemerAPI',
-    description: 'The best place to start your documentation.',
+    description: 'An awesome module that allows you to manipulate images very easily.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
       github: 'Yash094/Memer-API',
@@ -11,12 +11,15 @@ export default defineAppConfig({
       exclude: []
     },
     header: {
-      logo: true,
       showLinkIcon: true,
       exclude: []
     },
     footer: {
-
+      credits: {
+        icon: 'IconDocus',
+        text: 'Created by avalynndev',
+        href: 'https://github.com/avalynndev',
+      },
     }
   }
 })
