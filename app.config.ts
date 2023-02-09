d68425b9a47ac8d5b61df5787b629787e86b41bd
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-02-09 11:42:57 +0000

    Add Methods till letter B

--- app.config.ts
+++ app.config.ts
@@ -2,9 +2,13 @@ export default defineAppConfig({
   docus: {
     title: 'MemerAPI',
     description: 'An awesome module that allows you to manipulate images very easily.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    image: 'https://cdn.discordapp.com/attachments/801132434283954199/881571725106618428/memer_api.png',
     socials: {
       github: 'Yash094/Memer-API',
+    },
+    github: {
+      root: 'content',
+      edit: true,
     },
     aside: {
       level: 0,
